0347e271aff68f0f198be07363bc65a6afa7ce8d
王潇 <23601145@qq.com>
2016-02-26 21:34:14 +0800

    Initial Commit

---
+++ News_Bt_Jerry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News_Bt_Jerry
+//
+//  Created by 王潇 on 16/2/26.
+//  Copyright © 2016年 王潇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
